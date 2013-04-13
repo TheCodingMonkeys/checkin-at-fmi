8ab59810476ccb91307e43fc3045b82f3864806b
Ognyan Angelov <ognyan_angelov@yahoo.com>
2013-04-13 18:00:45 +0300

    templates + place capacity

--- checkinatfmi/places/models.py
+++ checkinatfmi/places/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class Place(models.Model):
     mac = models.CharField(max_length = 63)
     name = models.CharField(max_length = 15, null = True)
+    capacity = models.IntegerField()
+
