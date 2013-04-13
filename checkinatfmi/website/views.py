1765eb0dc0bb53b65b7af6ee3415bd2cb70c22f3
Ognyan Angelov <ognyan_angelov@yahoo.com>
2013-04-13 18:07:39 +0300

    render to response index

--- checkinatfmi/website/views.py
+++ checkinatfmi/website/views.py
@@ -1,5 +1,12 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
 from django.http import HttpResponse
 from django.http import Http404
 
+
 def index(request):
-    return HttpResponse("Pandichki!")
+    return render_to_response('index.html',
+            {
+            },
+            context_instance=RequestContext(request))
