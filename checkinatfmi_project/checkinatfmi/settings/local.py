7d3eed3250646183842298d988bedd679a8869fc
MightyPixel <ognyan_angelov@yahoo.com>
2013-12-04 02:02:49 +0200

    local database

--- checkinatfmi_project/checkinatfmi/settings/local.py
+++ checkinatfmi_project/checkinatfmi/settings/local.py
@@ -6,3 +6,15 @@ TEMPLATE_DEBUG = DEBUG
 
 EMAIL_HOST = "localhost"
 EMAIL_PORT = 1025
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': keychain.db_name,
+        'USER': keychain.db_user,
+        'PASSWORD': keychain.db_pass,
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
