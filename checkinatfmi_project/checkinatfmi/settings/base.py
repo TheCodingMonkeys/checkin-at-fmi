eacc7048a7e12ac09b155f161c52347ec3b02a9e
Ivaylo-Bachvarov <bachvarof@gmail.com>
2013-12-04 02:50:46 +0200

    Visual improvments

--- checkinatfmi_project/checkinatfmi/settings/base.py
+++ checkinatfmi_project/checkinatfmi/settings/base.py
@@ -30,7 +30,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': keychain.db_name,
         'USER': keychain.db_user,
         'PASSWORD': keychain.db_pass,
