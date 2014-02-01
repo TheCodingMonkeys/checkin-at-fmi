a4304e00d7717108b423eedc93e22b9c2b929a53
root <root@SusiAPI.(none)>
2014-02-01 14:20:24 +0200

    Add auto winter effects removing

--- checkinatfmi_project/website/static/js/checkin.js
+++ checkinatfmi_project/website/static/js/checkin.js
@@ -14,4 +14,10 @@ $(document).ready( function(){
 			$(this).addClass('progress-bar-info')
 		}
 	})
+
+	var dateNow = new Date();
+	if(dateNow.getMonth() >= 11 || dateNow.getMonth() == 0) {
+		$.fn.snow({minSize: 10, maxSize: 25, newOn: 700});
+		$('.container').add('<img id="christmas-panda" src="/static/images/panda_cut.png" alt="panda">');
+	}
 });
